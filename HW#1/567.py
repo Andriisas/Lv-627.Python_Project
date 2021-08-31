9aafa3d001df7a11c2efaf16ae15d588794ba01c
Andriisas <48296925+Andriisas@users.noreply.github.com>
2021-08-31 13:09:59 +0300

    change into functions

--- HW#1/567.py
+++ HW#1/567.py
@@ -1,18 +1,15 @@
-def simle_value(num):
-    count = 0
-    for x in range(1, num + 1):
-        if num % x == 0:
-            count += 1
-    if count <= 2:
-        return True
-    else:
-        return False
+def factorialof(value):
+    result = 1
+    for x in range(1, value + 1):
+        result *= x
+    return result
 
-def simple_divisors(n):
-    if n > 0 :
-        list_values = []
-        for x in range(1, n + 1):
-            if n % x == 0 and simle_value(x) == True:
-                list_values.append(x)
-
-        return list_values
+def three_consecutive(n):
+    listofnum = []
+    for x in range(1, factorialof(n) + 1):
+        listofnum.append(x)
+    for i in range(len(listofnum) - 2):
+        res = listofnum[i] * listofnum[i+1] * listofnum[i+2]
+        if factorialof(n) == res :
+            return True
+    return False
