0ef010ff0e844dbe6ade21f597fdc59cd09ebb5f
Andriisas <48296925+Andriisas@users.noreply.github.com>
2021-08-31 13:07:45 +0300

    Change into functions

--- HW#1/86-v.py
+++ HW#1/86-v.py
@@ -1,13 +1,10 @@
-class Task86V():
-    def __init__(self,value):
-        self.value = value
+def firstdigit(value):
+    if value > 0:
+        string_value = str(value)
+        list_values = []
+        for x in string_value:
+            list_values.append(int(x))
+        return list_values[0]
+    else:
+        return 'Enter Natural number!'
         
-    def firstdigit(self):
-        if self.value > 0:
-            string_value = str(self.value)
-            list_values = []
-            for x in string_value:
-                list_values.append(int(x))
-            return list_values[0]
-        else:
-            return 'Enter Natural number!'
